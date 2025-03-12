6a0a14d7f6a3ede0fb65771e2b56b282173efc62
Jyothi Sai Moganti <jyothisai@jyothis-mbp.mynetworksettings.com>
2025-03-11 22:02:12 -0400

    <relevant commit>

---
+++ budget_tracker/__init__.py
@@ -0,0 +1,7 @@
+"""
+Budget Tracker Package
+This package provides modules for handling database operations(db.py) and the command line interface(cli.py)
+for the Budget tracker app
+"""
+from .import db
+from .import cli
